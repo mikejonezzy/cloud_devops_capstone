3697015c15ccc4a7e8a41f07f6a058ee28bf2deb
Mike Jones <jonezzy@gmail.com>
2022-07-12 13:53:14 +0200

    Update app for new deployment

--- helloworld/app.py
+++ helloworld/app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def helloIndex():
-    return 'Hello World, my name is Mike Jones'
+    return 'Hello World, my name is Mike Jones, this is a new version'
 
 app.run(host='0.0.0.0', port=80)
