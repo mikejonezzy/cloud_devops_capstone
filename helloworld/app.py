e48e0e7893e7e3713159ec93c5966e146fc37de8
Mike Jones <jonezzy@gmail.com>
2022-07-20 14:16:15 +0100

    Rolling deployment version1

--- helloworld/app.py
+++ helloworld/app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def helloIndex():
-    return 'Hello World, my name is Mike Jones, this is a new version'
+    return 'Hello World, my name is Mike Jones, version 1'
 
 app.run(host='0.0.0.0', port=80)
